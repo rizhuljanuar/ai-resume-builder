0367fd82e94b96ec6be935c51e71d89eafd1225b
Rizhul Januar <rayjam66@gmail.com>
2024-06-27 22:35:49 +0700

    feat: resume preview section

--- src/dashboard/resume/components/ResumePreview.jsx
+++ src/dashboard/resume/components/ResumePreview.jsx
@@ -1,6 +1,10 @@
 import { ResumeInfoContext } from '@/context/ResumeInfoContext'
 import React, { useContext } from 'react'
 import PersonalDetailPreview from './preview/PersonalDetailPreview';
+import SummaryPreview from './preview/SummaryPreview';
+import ExperiencePreview from './preview/ExperiencePreview';
+import EducationalPreview from './preview/EducationalPreview';
+import SkillsPreview from './preview/SkillsPreview';
 
 function ResumePreview() {
   const { resumeInfo, setResumeInfo } = useContext(ResumeInfoContext);
@@ -15,12 +19,16 @@ function ResumePreview() {
       <PersonalDetailPreview resumeInfo={resumeInfo} />
 
       {/* Summary */}
+      <SummaryPreview resumeInfo={resumeInfo} />
 
       {/* Professional Experience */}
+      <ExperiencePreview resumeInfo={resumeInfo} />
 
       {/* Education */}
+      <EducationalPreview resumeInfo={resumeInfo} />
 
       {/* Skills */}
+      <SkillsPreview resumeInfo={resumeInfo} />
     </div>
   )
 }
